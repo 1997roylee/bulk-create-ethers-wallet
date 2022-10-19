9d71ee2d0cf0612d586063374ae3ce6a47b46b09
Roy <53046899+1997roylee@users.noreply.github.com>
2022-10-19 23:35:11 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useCallback, useState } from "react";
+import randombytes from "randombytes";
+import "./App.css";
+
+async function createWallet() {
+  return new Promise((resolve) => {
+    const id = randombytes(32).toString("hex");
+    const privateKey = "0x" + id;
+    // console.log("SAVE BUT DO NOT SHARE THIS:", privateKey);
+    resolve(new ethers.Wallet(privateKey));
+  }, 20);
+}
 
 function App() {
+  const [wallets, setWallets] = useState([]);
+  const [size, setSize] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const bulkCreateWallets = useCallback(async () => {
+    const walletList = [];
+    let n = size;
+    setIsLoading(true);
+    while(n > 0){
+      
+      n--;
+      walletList.push(await createWallet());
+    }
+
+    setIsLoading(false);
+    setWallets(walletList);
+  }, [size]);
+
+  const handleSizeChange = (event) => {
+    // size.current = parseInt(event.target.value);
+    setSize(parseInt(event.target.value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+      批量創建錢包
+        {isLoading? `Creating ${size} Wallets...` : null}
+        {/* {doneNumber.current}/{size} */}
+      </h1>
+      <input
+        placeholder="size of the wallets"
+        value={size}
+        type="number"
+        onChange={handleSizeChange}
+      />
+      <button onClick={bulkCreateWallets}>創建錢包</button>
+      <div>
+        {wallets.map((wallet, index) => (
+          <div key={`Wallet_${index}`} className='wallet'>
+            <p>Wallet {index}</p>
+            <p>Address: {wallet.address}</p>
+            <p>Private key: {wallet.privateKey}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
